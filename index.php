bc2eb04e305db52ff133a96f435926250f2d652e
aarekr <aare212@hotmail.com>
2025-02-02 17:28:36 +0200

    Footer with date

--- index.php
+++ index.php
@@ -5,4 +5,10 @@
     <body>
         <h1>Fakebook</h1>
     </body>
+    <div class="footer">
+        <?php echo date("D"); ?>
+        <?php echo date("d"); ?>
+        <?php echo date("M"); ?>
+        <?php echo date("Y"); ?>
+    </div>
 </html>
