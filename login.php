ad70c15785e5c9443715f86e7374c75837403eb6
aarekr <aare212@hotmail.com>
2025-02-06 14:53:14 +0200

    Login page items

--- login.php
+++ login.php
@@ -1,8 +1,21 @@
+<!DOCTYPE html>
 <html>
 <head>
     <title>Login</title>
 </head>
 <body>
     <h1>Login page</h1>
+    <form method="post" action="">
+        <p>
+            <label for="username">Username:</label>
+            <input type="text" name="username" id="username">
+        </p>
+        <p>
+            <label for="pwd">Password:</label>
+            <input type="password" name="pwd" id="pwd">
+        </p>
+        <p>
+            <input name="login" type="submit" value="Log in">
+        </p>
 </body>
 </html>
