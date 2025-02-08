6f6a8b6e6541e9fc21195bf1fedc43c8594743a6
aarekr <aare212@hotmail.com>
2025-02-08 08:11:23 +0200

    Password validation

--- register.php
+++ register.php
@@ -1,3 +1,26 @@
+<?php
+declare(strict_types = 1);
+$password = '';
+$message  = '';
+
+function valid_password(string $password): bool {
+    if (
+        mb_Strlen($password) >= 8
+        and preg_match('/[A-Z]/', $password)
+        and preg_match('/[a-z]/', $password)
+        and preg_match('/[0-9]/', $password)
+    ) {
+        return true;
+    }
+    return false;
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $password = $_POST['password'];
+    $valid = valid_password($password);
+    $message = $valid ? 'Password is valid' : 'Password not strong enough';
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
